d4eb15150ba4bfbbbd487c1057338f7dfc7d315f
DeepakQuazma123 <deepak.yadav@quazma.com>
2024-10-25 20:02:37 +0530

    new changes

--- prisma/migrations/20241024144638_init/migration.sql
+++ prisma/migrations/20241024144638_init/migration.sql
@@ -4,6 +4,17 @@ CREATE TABLE `Movie` (
     `title` VARCHAR(191) NOT NULL,
     `publishingYear` INTEGER NOT NULL,
     `poster` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20241025131438_change_updated_at/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `movie` ALTER COLUMN `updatedAt` DROP DEFAULT;
